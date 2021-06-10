5108fb05bfddd7aa69675abb35ea5d8f8d90e7ad
cameronapriest <cpriest@calpoly.edu>
2021-06-09 21:16:11 -0700

    works way better in the dark

--- main.cpp
+++ main.cpp
@@ -1,44 +1,86 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <iostream>             // for cout
 #include <JetsonGPIO.h>         // for GPIO in C++
-#include <unistd.h>		// for sleep()
+#include <unistd.h>		        // for sleep()
 
 #define STOP_PIN 11			// blue wire
 #define GO_PIN 12	 		// green wire
 #define LEFT_PIN 15			// purple wire
-#define RIGHT_PIN 18			// purple wire
-#define RECENTER_PIN 13			// black wire
-#define GANTRY_RECENTERED_PIN 19 	// black wire
+#define RIGHT_PIN 18		// purple wire
+#define RECENTER_PIN 13		// black wire
 
 #define SHAVEOFF_SIDES 200
 #define GROUND_SAFETY 20
 
 #define CLOSE_DIST 1
-#define FAR_DIST 2.3
-#define PERSON_PIXELS 1000
+#define FAR_DIST 1.5
 
-#define LEFT_THRESHOLD 100
-#define RIGHT_THRESHOLD 100
+#define LEFT_THRESHOLD 4000 //100
+#define RIGHT_THRESHOLD 4000 //100
 
 #define YES 1
 #define NO 0
 
 uint8_t personInCenter(rs2::depth_frame depth);
+int maxOfFour(int * array);
+
+int maxOfFour(int * array) {
+    int max = array[0];
+    int returnIndex = 0;
+    for (int i = 1; i < 4; i++) {
+        if (array[i] > max) {
+            max = array[i];
+            returnIndex = i;
+        }
+    }
+
+    return returnIndex;
+}
 
 uint8_t personInCenter(rs2::depth_frame depth, float width, float height) {
     int i, j;
-    int pixels = 0;
+    int leftPoints = 0, goPoints = 0, stopPoints = 0, rightPoints = 0;
     float dist;
+
+    for (i = 0; i < SHAVEOFF_SIDES; i += 2) {
+        for (j = GROUND_SAFETY; j < height; j += 2) {
+            dist = depth.get_distance(i, j);
+            if (dist > CLOSE_DIST && dist < FAR_DIST) {
+                leftPoints++;
+            }
+        }       
+    }
+
     for (i = SHAVEOFF_SIDES; i < width-SHAVEOFF_SIDES; i += 2) {
         for (j = GROUND_SAFETY; j < height; j += 2) {
             dist = depth.get_distance(i, j);
             if (dist > CLOSE_DIST && dist < FAR_DIST) {
-                pixels++;
+                goPoints++;
+            } else if (dist < CLOSE_DIST && dist > 0) {
+                stopPoints++;
             }
         }
     }
-    if (pixels > 100) {
-	return YES;
+
+    for (i = width-SHAVEOFF_SIDES; i < width; i += 2) {
+        for (j = GROUND_SAFETY; j < height; j += 2) {
+            dist = depth.get_distance(i, j);
+            if (dist > CLOSE_DIST && dist < FAR_DIST) {
+                rightPoints++;
+            }
+        }
+    }
+
+    int arr[4];
+    arr[0] = leftPoints;
+    arr[1] = rightPoints;
+    arr[2] = goPoints;
+    arr[3] = stopPoints;
+    int index = maxOfFour(arr);
+    printf("left: %d go: %d stop: %d right: %d\n", leftPoints, goPoints, stopPoints, rightPoints);
+
+    if (index == 2) {
+        return YES;
     }
 
     return NO;
@@ -52,41 +94,6 @@ int main(int argc, char * argv[]) try {
     GPIO::setup(LEFT_PIN, GPIO::OUT, GPIO::LOW);
     GPIO::setup(RIGHT_PIN, GPIO::OUT, GPIO::LOW);
     GPIO::setup(RECENTER_PIN, GPIO::OUT, GPIO::LOW);
-    //GPIO::setup(GANTRY_RECENTERED_PIN, GPIO::IN);
-    GPIO::setup(19, GPIO::IN);
-
-    /*printf("GPIO test");
-    GPIO::output(GO_PIN, 1);
-    sleep(0.1);
-    //GPIO::output(GO_PIN, 0);
-
-    GPIO::output(STOP_PIN, 1);
-    sleep(0.1);
-    //GPIO::output(STOP_PIN, 0);
-
-    GPIO::output(LEFT_PIN, 1);
-    sleep(0.1);
-    //GPIO::output(LEFT_PIN, 0);
-
-    GPIO::output(RIGHT_PIN, 1);
-    sleep(0.1);
-    //GPIO::output(RIGHT_PIN, 0);
-
-    GPIO::output(RECENTER_PIN, 1);
-    sleep(0.1);
-    //GPIO::output(RECENTER_PIN, 0);
-
-    while (1) {
-	int val = GPIO::input(19); //GANTRY_RECENTERED_PIN);
-	if (val == GPIO::HIGH) {
-	    printf("high\n");
-	} else if (val == GPIO::LOW) {
-	    printf("low\n");
-	} else {
-	    printf("gantry recentered pin is: %d\n", val);
-	}
-	sleep(1);
-    }*/
 
     // Create a Pipeline - this serves as a top-level API for streaming and processing frames
     rs2::pipeline p;
@@ -97,9 +104,7 @@ int main(int argc, char * argv[]) try {
     std::cout << "Vision System is up and running\n";
 
     int warmups = 0; // "warm up" iterations to ensure data is valid
-    int prevDidwebreak = -1;
-    int i, j, didwebreak;
-    int goPoints, stopPoints, leftPoints, rightPoints;
+    int i, j, goPoints, stopPoints, leftPoints, rightPoints;
     float dist;
 
     while (true) {
@@ -117,87 +122,137 @@ int main(int argc, char * argv[]) try {
         //std::cout << "\nheight in pixels: " << height;
 
         goPoints = 0; // number of points that could be a person
-	stopPoints = 0;
-	leftPoints = 0;
-	rightPoints = 0;
-	dist = 0;
-	warmups++;
+	    stopPoints = 0;
+	    leftPoints = 0;
+	    rightPoints = 0;
+	    dist = 0;
+	    warmups++;
 
         if (warmups > 30) {
-            didwebreak = 0;
 
-	    for (i = 0; i < SHAVEOFF_SIDES; i += 2) {
-		for (j = GROUND_SAFETY; j < height; j += 2) {
-		    dist = depth.get_distance(i, j);
-		    if (dist > CLOSE_DIST && dist < FAR_DIST) {
-			leftPoints++;
-		    }
-		}
-	    }
+	        for (i = 0; i < SHAVEOFF_SIDES; i += 2) {
+		        for (j = GROUND_SAFETY; j < height; j += 2) {
+		            dist = depth.get_distance(i, j);
+		            if (dist > CLOSE_DIST && dist < FAR_DIST) {
+			            leftPoints++;
+		            }
+		        }       
+	        }
 
             for (i = SHAVEOFF_SIDES; i < width-SHAVEOFF_SIDES; i += 2) {
                 for (j = GROUND_SAFETY; j < height; j += 2) {
                     dist = depth.get_distance(i, j);
-		    if (dist > CLOSE_DIST && dist < FAR_DIST) {
-			goPoints++;
-		    } else if (dist < CLOSE_DIST && dist > 0) {
-			stopPoints++;
-		    }
+		            if (dist > CLOSE_DIST && dist < FAR_DIST) {
+			            goPoints++;
+		            } else if (dist < CLOSE_DIST && dist > 0) {
+			            stopPoints++;
+		            }
                 }
             }
 
-	    for (i = width-SHAVEOFF_SIDES; i < width; i += 2) {
-		for (j = GROUND_SAFETY; j < height; j += 2) {
-		    dist = depth.get_distance(i, j);
-		    if (dist > CLOSE_DIST && dist < FAR_DIST) {
-			rightPoints++;
-		    }
-		}
-	    }
+	        for (i = width-SHAVEOFF_SIDES; i < width; i += 2) {
+		        for (j = GROUND_SAFETY; j < height; j += 2) {
+		            dist = depth.get_distance(i, j);
+		            if (dist > CLOSE_DIST && dist < FAR_DIST) {
+			            rightPoints++;
+		            }
+		        }
+	        }
 
-	    //printf("left: %d go: %d stop: %d right: %d\n", leftPoints, goPoints, stopPoints, rightPoints);
+	        //printf("left: %d go: %d stop: %d right: %d\n", leftPoints, goPoints, stopPoints, rightPoints);
 
-	    if (leftPoints < LEFT_THRESHOLD && rightPoints < RIGHT_THRESHOLD) {
-		if (std::max(goPoints, stopPoints) == goPoints) {
-		    printf("go\n");
-		    GPIO::output(GO_PIN, GPIO::HIGH);
-		    sleep(0.1);
-		    GPIO::output(GO_PIN, GPIO::LOW);
-		} else {
-		    printf("stop\n");
-		    GPIO::output(STOP_PIN, GPIO::HIGH);
-		    sleep(0.1);
-		    GPIO::output(STOP_PIN, GPIO::LOW);
-		}
-	    } else if (leftPoints > LEFT_THRESHOLD && rightPoints < RIGHT_THRESHOLD) {
-		printf("left\n");
-		GPIO::output(LEFT_PIN, GPIO::HIGH);
-		while (personInCenter(depth, width, height) == NO) {
-		    printf("waiting for person to be in the center again\n");
-		}
-		printf("person recentered!\n");
-		GPIO::output(RECENTER_PIN, GPIO::HIGH);
-		sleep(0.1);
-		GPIO::output(LEFT_PIN, GPIO::LOW);
-		GPIO::output(RECENTER_PIN, GPIO::LOW);
-		//block until GANTRY_RECENTERED_PIN is set high 
-	    } else if (leftPoints < LEFT_THRESHOLD && rightPoints > RIGHT_THRESHOLD) {
-		printf("right\n");
-	    } else {
-		printf("BOTH LEFT AND RIGHT... ignoring\n");
-	    }
+            int arr[4];
+            arr[0] = leftPoints;
+            arr[1] = rightPoints;
+            arr[2] = goPoints;
+            arr[3] = stopPoints;
+            int index = maxOfFour(arr);
 
-            /*if (goPoints > PERSON_PIXELS) { // 20+ pixels indicate valid hazard
-                didwebreak = 1;
+            if (index == 2) {
+                printf("go\n");
+		        GPIO::output(GO_PIN, GPIO::HIGH);
+		        sleep(0.1);
+	            GPIO::output(GO_PIN, GPIO::LOW);
+
+            } else if (index == 3) {
+                printf("stop\n");
+                GPIO::output(STOP_PIN, GPIO::HIGH);
+                sleep(0.1);
+                GPIO::output(STOP_PIN, GPIO::LOW);
+
+            } else if (index == 0) {
+                printf("left\n");
+		        GPIO::output(LEFT_PIN, GPIO::HIGH);
+		        uint8_t result = NO;
+		        while (result != YES) {
+
+                    rs2::frameset currentFrames = p.wait_for_frames();
+                    rs2::depth_frame currentDepth = currentFrames.get_depth_frame();
+
+                    float currentWidth = currentDepth.get_width();
+                    float currentHeight = currentDepth.get_height();
+
+                    result = personInCenter(currentDepth, currentWidth, currentHeight);
+		            printf("waiting for person to be in the center again\n");
+		        }
+		        printf("person recentered!\n");
+		        GPIO::output(RECENTER_PIN, GPIO::HIGH);
+		        sleep(0.1);
+		        GPIO::output(LEFT_PIN, GPIO::LOW);
+		        GPIO::output(RECENTER_PIN, GPIO::LOW);
+            } else if (index == 1) {
+                printf("right\n");
+		        GPIO::output(RIGHT_PIN, GPIO::HIGH);
+                uint8_t result = NO;
+		        while (result != YES) {
+
+                    rs2::frameset currentFrames = p.wait_for_frames();
+                    rs2::depth_frame currentDepth = currentFrames.get_depth_frame();
+
+                    float currentWidth = currentDepth.get_width();
+                    float currentHeight = currentDepth.get_height();
+
+                    result = personInCenter(currentDepth, currentWidth, currentHeight);
+		            printf("waiting for person to be in the center again\n");
+		        }
+		        printf("person recentered!\n");
+		        GPIO::output(RECENTER_PIN, GPIO::HIGH);
+		        sleep(0.1);
+		        GPIO::output(RIGHT_PIN, GPIO::LOW);
+		        GPIO::output(RECENTER_PIN, GPIO::LOW);
             }
 
-            //std::cout << didwebreak << "\n";
-	    if ((prevDidwebreak != didwebreak) || (prevDidwebreak == -1)) {
-		std::cout << didwebreak << "\n";
-		GPIO::output(GO_PIN, didwebreak); // output 1 if hazard, else 0
-	    }*/
-
-	    prevDidwebreak = didwebreak;
+	        /*if ((leftPoints < LEFT_THRESHOLD && rightPoints < RIGHT_THRESHOLD) ||
+		        (leftPoints > LEFT_THRESHOLD && rightPoints > RIGHT_THRESHOLD)) {
+		        if (std::max(goPoints, stopPoints) == goPoints) {
+		            printf("go\n");
+		            GPIO::output(GO_PIN, GPIO::HIGH);
+		            sleep(0.1);
+		            GPIO::output(GO_PIN, GPIO::LOW);
+		        } else {
+		            printf("stop\n");
+		            GPIO::output(STOP_PIN, GPIO::HIGH);
+		            sleep(0.1);
+		            GPIO::output(STOP_PIN, GPIO::LOW);
+		        }
+	        } else if (leftPoints > LEFT_THRESHOLD && rightPoints < RIGHT_THRESHOLD) {
+		        printf("left\n");
+		        GPIO::output(LEFT_PIN, GPIO::HIGH);
+		        while (personInCenter(depth, width, height) == NO) {
+		            printf("waiting for person to be in the center again\n");
+		        }
+		        printf("person recentered!\n");
+		        GPIO::output(RECENTER_PIN, GPIO::HIGH);
+		        sleep(0.1);
+		        GPIO::output(LEFT_PIN, GPIO::LOW);
+		        GPIO::output(RECENTER_PIN, GPIO::LOW);
+		        //block until GANTRY_RECENTERED_PIN is set high 
+	        } else if (leftPoints < LEFT_THRESHOLD && rightPoints > RIGHT_THRESHOLD) {
+		        printf("right\n");
+	        } else {
+		        printf("BOTH LEFT AND RIGHT... ignoring\n");
+		        printf("left: %d go: %d stop: %d right: %d\n", leftPoints, goPoints, stopPoints, rightPoints);
+	        }*/
         }
     }
 
